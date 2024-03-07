0862c0fe66dc48dbe7cb45f223e17daf63cc6c33
Satpal Singh Rathore <satpal.code@gmail.com>
2024-03-07 17:44:57 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import dotenv from "dotenv";
+import pkg from "./package.json" assert { type: "json" };
 
 dotenv.config({ path: "./.env.local" });
 dotenv.config({ path: "./.env" });
 
-process.env.PUBLIC_VERSION = process.env.npm_package_version;
+process.env.PUBLIC_VERSION = pkg.version.replace(/\.0\b/g, "");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,14 +15,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// see the 'Deployment configuration' section below
+		}),
 
 		paths: {
 			base: process.env.APP_BASE || "",
-		},
-		csrf: {
-			// handled in hooks.server.ts, because we can have multiple valid origins
-			checkOrigin: false,
 		},
 	},
 };
